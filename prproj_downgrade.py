64cb3cf695232e4e848a043daeb23fffc4e496ef
snorkem <snorkem@gmail.com>
2019-12-31 13:16:49 -0500

    Added more comments and simplified some variables.

--- prproj_downgrade.py
+++ prproj_downgrade.py
@@ -10,7 +10,11 @@ import os
 
 def install(package):  # Install required modules if not present.
     if sys.platform == 'darwin':
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
+        try:
+            subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
+        except Exception as e:
+            print('Encountered exception: ' + str(e))
+            print('Error installing modules. Quiting.')
     # if sys.platform == 'win32' or sys.platform == 'cygwin':   # Check for windows machine. May be be unnecessary.
     else:
         print('Error installing modules. Quiting.')
@@ -58,6 +62,7 @@ def downgrade(prproj_in, version='1'):  # Main functionality of the program. Dow
     :param version: Optional. Specify what version to downgrade to. Defaults to version '1'.
     """
     new_version = version
+    new_name = prproj_in + '_DOWNGRADED' + '(v.' + str(new_version) + ').prproj'
     root, ext = os.path.splitext(prproj_in)  # Checking if file extension is correct.
     try:
         if ext != '.prproj':
@@ -72,13 +77,13 @@ def downgrade(prproj_in, version='1'):  # Main functionality of the program. Dow
                 soup.Project.find_next()['Version'] = new_version  # Change project version number to 1
                 print('Downgraded project version to: ' +
                       str(soup.Project.find_next()['Version']))  # Print new current version.
-                if os.path.exists(prproj_in + '_DOWNGRADED.prproj'):
+                if os.path.exists(new_name):
                     print('Output file already exists at this location. Please move or rename.')
                     exit(-1)
                 else:
-                    with gzip.open(prproj_in + '_DOWNGRADED.prproj', 'wt') as f_out:
+                    with gzip.open(new_name, 'wt') as f_out:
                         f_out.write(soup.prettify())  # Turn soup object to string for final writing to gzip file.
-                        print('Downgrade Complete. New file: ' + prproj_in + '_DOWNGRADED.prproj')  # Change file extension.
+                        print('Downgrade Complete. New file: ' + new_name)  # Change file extension.
     except:
         exception = sys.exc_info()
         handle_exceptions(exception[0])
@@ -86,4 +91,3 @@ def downgrade(prproj_in, version='1'):  # Main functionality of the program. Dow
 
 if __name__ == '__main__':
     fire.Fire()
-
