a774d63b0f3330d0b6d392ce7960807d78a1dfcf
snorkem <snorkem@gmail.com>
2020-02-15 10:44:20 -0800

    Added watch folder functionality.

--- prproj_downgrade.py
+++ prproj_downgrade.py
@@ -4,7 +4,7 @@ Downgrade Adobe Premiere Pro project files.
 
 Simple script for downgrading Adobe Premiere Pro project files to version 1. Tested on Macs only at this time.\n
 Downgraded files should be able to open with any newer version of Premiere.
-Version: 0.8
+Version: 1.0
 # by Alex Fichera.
 Example Usage: prproj_downgrade.py downgrade <path-to-file>
 or:            prproj_downgrade.py info <path-to-file>
@@ -16,6 +16,7 @@ import sys
 import subprocess
 try:  # Trying to do the rest of the imports. We will need these all later.
     import shutil
+    import time
     import gzip
     import re
     import fire
@@ -25,10 +26,13 @@ try:  # Trying to do the rest of the imports. We will need these all later.
     from termcolor import cprint
     from tqdm import tqdm
     from tqdm import trange
+    from progress.spinner import PixelSpinner
     from pathlib import Path
+    from watchdog.observers import Observer
+    from watchdog.events import FileSystemEventHandler
 except ImportError:
-    print('Non-standard modules not found. Attempting to install...')
-    packages = ['fire', 'tqdm', 'colorama', 'termcolor', 'pyfiglet']  # Non-native required packages.
+    print('One or more non-standard modules not found. Attempting to install...')
+    packages = ['fire', 'tqdm', 'colorama', 'termcolor', 'pyfiglet', 'progress', 'watchdog']  # Non-native required packages.
     for p in packages:
         if sys.platform == 'darwin':
             try:
@@ -36,9 +40,9 @@ except ImportError:
             except Exception as e:
                 print('Encountered exception: ' + str(e))
                 print('Error installing modules. Quiting.')
-        elif sys.platform == 'win32' or sys.platform == 'cygwin':
+        elif sys.platform == 'win32' or sys.platform == 'cygwin':  # Check for windows machine. May be be unnecessary.
             try:
-                subprocess.check_call([sys.executable, '-m', 'pip', 'install', p])  # Check for windows machine. May be be unnecessary.
+                subprocess.check_call([sys.executable, '-m', 'pip', 'install', p])
             except Exception as e:
                 print('Encountered exception: ' + str(e) + '\nError installing modules. Quiting.')
         else:
@@ -46,6 +50,40 @@ except ImportError:
             exit()
     print('\n...\nSuccess!\nExiting program. Should now work if you run it again.')
 # --- End of imports --- #
+# --- Begin class definitions --- #
+
+
+class MyEventHandler(FileSystemEventHandler):
+    # Define event handler for watchdog module.
+    # This will be called when user wants to watch a given directory for changes.
+    def __init__(self, watch_dir, output_dir):
+        self.watch_dir = watch_dir
+        self.output_dir = output_dir
+
+        if os.path.exists(self.output_dir) is False:
+            os.mkdir(self.output_dir)
+        elif os.path.exists(self.watch_dir) is False:
+            print('Watch directory not found. Check your path.')
+            exit()
+
+    def on_modified(self, event):
+        files = [f for f in os.listdir(self.watch_dir) if (os.path.isfile(os.path.join(self.watch_dir, f)))
+                 if (os.path.join(self.watch_dir, f).endswith('.prproj'))]
+        files[:] = [self.watch_dir + file for file in files]
+        if len(files) > 0:
+            print(files)
+            latest_file = max(files, key=os.path.getmtime)
+            print('test ' + latest_file)
+            '''for file in files:
+                downgrade(file, output_dir=self.output_dir)'''
+            if latest_file:
+                downgrade(latest_file, output_dir=self.output_dir)
+                for file in files:
+                    os.rename(file, file + '.processed')
+                latest_file = None
+        else:
+            return
+# --- End class definitions --- #
 
 
 def handle_exceptions(exception):  # Receives an exception type and does error handling.
@@ -71,27 +109,6 @@ def is_project_objectID(line):
         return True
     else:
         return False
-
-
-def info(prproj_in):  # Fetches the project version from the target .prproj file.
-    try:
-        root, ext = os.path.splitext(prproj_in)  # Checking if file extension is correct.
-        if ext != '.prproj':
-            print('Invalid filetype. Must have valid .prproj extension.')  # If not a valid Adobe Premiere file, exit.
-            exit()
-        with gzip.open(prproj_in, 'rt') as f:
-            search_string = '<PresetPath>/Applications/'
-            build_line_search = '<MZ.BuildVersion.Created>'
-            lines = f.readlines()
-            for line in lines:
-                if search_string in line:
-                    version = re.search(r'Adobe\sPremiere\sPro\s\d\d\d\d', line)
-                elif build_line_search in line:
-                    build_version = re.search(r'\d\d[\.]\d[.]\d', line)
-            print('Project Created in: ' + version.group() + '\n' + 'Build Version: ' + build_version.group())
-    except:
-        exception = sys.exc_info()
-        handle_exceptions(exception)
 
 
 def unzip_file(f_in, f_out):
@@ -148,7 +165,7 @@ def modify_xml(tmp_file_out, xml_contents):
             handle_exceptions(exception)
 
 
-def downgrade(prproj_in):  # Main functionality of the program. Downgrades target prproj files.
+def downgrade(prproj_in, output_dir=''):  # Main functionality of the program. Downgrades target prproj files.
     """
     Downgrade Adobe Premiere Pro project files.
 
@@ -157,14 +174,19 @@ def downgrade(prproj_in):  # Main functionality of the program. Downgrades targe
     Author: Alex Fichera
     Version: 0.8
     :param prproj_in: path to Premiere Pro project file
+    :param output_dir: optional, destination output directory
     """
     new_version = '1'
     root, ext = os.path.splitext(prproj_in)  # Checking if file extension is correct.
-    new_name = (root + '_DOWNGRADED' + '(v.' + str(new_version) + ').prproj')
+    src_file_name = os.path.basename(root)
+    new_name = ((root if output_dir == '' else output_dir) + src_file_name +
+                '_DOWNGRADED' + '(v.' + str(new_version) + ').prproj')
+    print('new_name is: {new_name}'.format(new_name=new_name))
     temp_name = os.path.split(root)[0] + '/prproj_downgrade.tmp'
 
     try:
-        welcome()
+        if output_dir == '':
+            welcome()
         if ext != '.prproj':
             print('Invalid filetype. Must have valid .prproj extension.')
             # If not a valid Adobe Premiere file, exit.
@@ -174,7 +196,7 @@ def downgrade(prproj_in):  # Main functionality of the program. Downgrades targe
             xml_contents = unzip_file(prproj_in, temp_name)
             modify_xml(temp_name, xml_contents)
             zip_file(temp_name, new_name)
-            print('Cleaning up temp file "%s"...' % temp_name)
+            print('Cleaning up temp file {temp_name}...'.format(temp_name=temp_name))
             os.remove(temp_name)
             print('Success! Project file downgraded at: ' + new_name)
     except:
@@ -182,8 +204,55 @@ def downgrade(prproj_in):  # Main functionality of the program. Downgrades targe
         handle_exceptions(exception)
 
 
+def info(prproj_in):  # Fetches the project version from the target .prproj file.
+    try:
+        root, ext = os.path.splitext(prproj_in)  # Checking if file extension is correct.
+        if ext != '.prproj':
+            print('Invalid filetype. Must have valid .prproj extension.')  # If not a valid Adobe Premiere file, exit.
+            exit()
+        with gzip.open(prproj_in, 'rt') as f:
+            search_string = '<PresetPath>/Applications/'
+            build_line_search = '<MZ.BuildVersion.Created>'
+            lines = f.readlines()
+            for line in lines:
+                if search_string in line:
+                    version = re.search(r'Adobe\sPremiere\sPro\sCC\s\d\d\d\d', line)
+                elif build_line_search in line:
+                    build_version = re.search(r'\d\d[\.]\d[.]\d', line)
+            if version and build_version:
+                print('Project Created in: ' + version.group() + '\n' + 'Build Version: ' + build_version.group())
+            else:
+                print('Project info not found.')
+    except:
+        exception = sys.exc_info()
+        handle_exceptions(exception)
+
+
+def watch(watch_dir, output_dir):
+    """
+    :param watch_dir: Directory to watch for new Premiere Pro project files.
+    :param output_dir: Directory to place downgraded Premiere Pro project files.
+    :return:
+    """
+    event_handler = MyEventHandler(watch_dir, output_dir)  # instantiate event handler class from begining of file
+    observer = Observer()
+    observer.schedule(event_handler, path=watch_dir, recursive=False)  # set the observer parameters
+    observer.start()  # start watching the directory
+    try:
+        with PixelSpinner('...Watching target directory...') as bar:
+            while True:
+                time.sleep(0.06)
+                bar.next()
+        while True:
+            time.sleep(2)
+    except KeyboardInterrupt:
+        observer.stop()
+        observer.join()
+
+
 if __name__ == '__main__':
     fire.Fire({
         'project_info': info,
         'downgrade': downgrade,
+        'watch': watch
     })
